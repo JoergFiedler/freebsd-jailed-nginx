6fe2280d3ca1b30c5c43bfbb27d198b228e88954
Joerg John Fiedler <joerg.fiedler@moumantai.de>
2017-01-26 02:33:58 +0100

    use lets encrypt for ssl certificates

---
+++ templates/acme-client-weekly.j2.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+SSLDIR="/usr/local/etc/nginx/ssl"
+DOMAINSFILE="{{ nginx_letsencrypt_domains_file }}"
+CHALLENGEDIR="/var/www/.well-known/acme-challenge"
+
+[ ! -d "${SSLDIR}/private" ] && mkdir -pm700 "${SSLDIR}/private"
+
+cat "${DOMAINSFILE}" | while read domain line ; do
+   CERTSDIR="${SSLDIR}/${domain}"
+   [ ! -d "${CERTSDIR}" ] && mkdir -pm755 "${CERTSDIR}"
+   set +e # RC=2 when time to expire > 30 days
+   acme-client -C "${CHALLENGEDIR}" \
+               -k "${CERTSDIR}/key.pem" \
+               -c "${CERTSDIR}" \
+               -bnNv \
+               ${domain} ${line}
+   RC=$?
+   set -e
+   [ $RC -ne 0 -a $RC -ne 2 ] && exit $RC
+done
